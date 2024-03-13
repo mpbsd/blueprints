869c108ab8ce5cae817d1ccfeccb8aaea8770e4c
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2024-03-13 12:00:59 -0300

    add separate functions to execute different tasks

--- pkgs/core.py
+++ pkgs/core.py
@@ -5,7 +5,7 @@
 from datetime import date, timedelta
 
 
-non_labor_days = [
+lazy_days = [  # {{{
     date.fromisoformat("2024-03-29"),  # paixao de cristo
     date.fromisoformat("2024-04-08"),  # vii coloquio do centro-oeste
     date.fromisoformat("2024-04-10"),  # vii coloquio do centro-oeste
@@ -15,11 +15,12 @@ non_labor_days = [
     date.fromisoformat("2024-05-24"),  # padroeira de goiania
     date.fromisoformat("2024-05-30"),  # corpus christi
     date.fromisoformat("2024-05-31"),  # ponto facultativo corpus christi
-]
+]  # }}}
 
 
 discipline = {
-    "analise_real_1": {
+    "IME0388": {
+        "dname": "Análise Real 1",
         "crono": [  # {{{
             "Apresentação da disciplina",
             "Noções Sobre Conjuntos e Funções",
@@ -69,28 +70,116 @@ discipline = {
             "Avaliação 3",
             "Conclusão da disciplina",
         ],  # }}}
-        "shift": {
+        "shift": {  # {{{
             1: timedelta(days=2),
             3: timedelta(days=2),
             5: timedelta(days=3),
-        },
+        },  # }}}
+    },
+    "IME0415": {
+        "dname": "Espaços Métricos",
+        "crono": [  # {{{
+            "Espaços métricos: Definição e exemplos",
+            "Espaços métricos: Bolas e esferas",
+            "Espaços métricos: Conjuntos limitados",
+            "Espaços métricos: Distânica entre conjuntos",
+            "Espaços métricos: Isometrias",
+            "Espaços métricos: Pseudo-métricas",
+            "Funções Contínuas: Definição e exemplos",
+            "Funções Contínuas: Propriedades elementares",
+            "Funções Contínuas: Homeomorfismos",
+            "Funções Contínuas: Métricas equivalentes",
+            "Funções Contínuas: Transformações multilineares",
+            "Topologia: Conjuntos abertos",
+            "Topologia: Conjuntos abertos e continuidade",
+            "Topologia: Espaços topológicos",
+            "Topologia: Conjuntos fechados",
+            "Conjuntos conexos: Definição e exemplos",
+            "Conjuntos conexos: Propriedades dos conjuntos conexos",
+            "Conjuntos conexos: Conexidade por caminhos",
+            "Conjuntos conexos: Componentes conexas",
+            "Conjuntos conexos: A conexidade como invariante topológico",
+            "Limites: Limites de sequências",
+            "Limites: Sequências de números reais",
+            "Limites: Séries",
+            "Limites: Convergência e topologia",
+            "Limites: Sequências de funções",
+            "Limites: Produtos cartesianos infinitos",
+            "Limites: Limites de funções",
+            "Continuidade uniforme: Observações e exemplos",
+            "Espaços completos: Sequências de Cauchy",
+            "Espaços completos: Espaços métricos completos",
+            "Espaços completos: Espaços de Banach e espaços de Hilbert",
+            "Espaços completos: Extensão de aplicações contínuas",
+            "Espaços completos: Completamento de um espaço métrico",
+            "Espaços completos: Espaços métricos topologicamente completos",
+            "Espaços completos: O Teorema de Baire",
+            "Espaços completos: O método das aproximações sucessivas",
+            "Espaços compactos: Compacidade na reta",
+            "Espaços compactos: Espaços métricos compactos",
+            "Espaços compactos: Produtos de dois fatores, um dos quais é compacto",
+            "Espaços compactos: Uma base para $TOPIC(K,M)$",
+            "Espaços compactos: Caracterizações de espaços compactos",
+            "Espaços compactos: Produtos cartesianos de espaços compactos",
+            "Espaços compactos: Continuidade uniforme",
+            "Espaços compactos: Espaços localmente compactos",
+            "Espaços compactos: Espaços vetoriais normados de dimensão finita",
+            "Espaços compactos: Equicontinuidade",
+            "Espaços compactos: O Teorema de Stone-Weierstrass",
+        ],  # }}}
+        "shift": {  # {{{
+            1: timedelta(days=2),
+            3: timedelta(days=2),
+            5: timedelta(days=3),
+        },  # }}}
     },
 }
 
 
+def pprint(DATE, TOPIC):
+    M = [
+        "Jan",
+        "Fev",
+        "Mar",
+        "Abr",
+        "Mai",
+        "Jun",
+        "Jul",
+        "Ago",
+        "Set",
+        "Out",
+        "Nov",
+        "Dez",
+    ]
+    W = ["Do", "Se", "Te", "Qa", "Qi", "Sx", "Sa"]
+    y = DATE.year
+    m = M[DATE.month]
+    d = "%02d" % DATE.day
+    w = W[DATE.isoweekday() % 7]
+    R = f"({w}) {d}/{m}/{y}: {TOPIC}"
+    print(R)
+
+
 def printcrono(discipline, opening):
-    Date = opening
-    for C in discipline["crono"]:
-        if Date in non_labor_days:
-            while Date in non_labor_days:
-                Date += discipline["shift"][Date.isoweekday() % 7]
-        print(Date, C)
-        Date += discipline["shift"][Date.isoweekday() % 7]
+    DATE = opening
+    for TOPIC in discipline["crono"]:
+        if DATE in lazy_days:
+            while DATE in lazy_days:
+                DATE += discipline["shift"][DATE.isoweekday() % 7]
+        pprint(DATE, TOPIC)
+        DATE += discipline["shift"][DATE.isoweekday() % 7]
+
+
+def beancount(discipline):
+    print(2 * len(discipline["crono"]))
 
 
 def main():
     opening = date.fromisoformat("2024-03-18")
-    printcrono(discipline["analise_real_1"], opening)
+    printcrono(discipline["IME0388"], opening)
+    beancount(discipline["IME0388"])
+    # printcrono(discipline["IME0415"], opening)
+    # beancount(discipline["IME0415"])
 
 
 if __name__ == "__main__":
